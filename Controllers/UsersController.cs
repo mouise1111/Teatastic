4495067a4803ba6c7539e1d7bd7fa1bef5c35483
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-01-02 00:34:22 +0100

    User controller + views added

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Teatastic.Areas.Identity.Data;
+
+namespace Teatastic.Controllers
+{
+    [Authorize(Roles = "UserAdministrator")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<TeatasticUser> _userManager;
+
+        public UsersController(UserManager<TeatasticUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TeatasticUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                // You may want to add additional logic here to create the user
+                // For example, you may want to generate a random password and set it
+                // for the user
+                var result = await _userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "An error occurred while creating the user");
+                }
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, TeatasticUser user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "An error occurred while updating the user");
+                }
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ModelState.AddModelError("", "An error occurred while deleting the user");
+            }
+            return View(user);
+        }
+    }
+}
