09aadc58339217329855182b762229cb849510ab
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-01-11 07:38:24 +0100

    order model and controller added

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Teatastic.Data;
+using Teatastic.Models;
+
+namespace Teatastic.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly TeatasticContext _context;
+        private readonly Cart _cart;
+
+        public OrderController(TeatasticContext context, Cart cart)
+        {
+            _context = context;
+            _cart = cart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var cartItems = _cart.GetAllCartItems();
+            _cart.CartItems = cartItems;
+
+            if (_cart.CartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Cart is empty, please add a tea first.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                CreateOrder(order);
+                _cart.ClearCart();
+                return View("CheckoutComplete", order);
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete(Order order)
+        {
+            return View(order);
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            var cartItems = _cart.CartItems;
+
+            foreach (var item in cartItems)
+            {
+                var orderItem = new OrderItem()
+                {
+                    Quantity = item.Quantity,
+                    TeaId = item.Tea.Id,
+                    OrderId = order.Id,
+                    Price = (int)(item.Tea.Price * item.Quantity)
+                };
+                order.OrderItems.Add(orderItem);
+                order.OrderTotal += orderItem.Price;
+            }
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Models/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Teatastic.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+        public int OrderId { get; set; }
+        public int TeaId { get; set; }
+        public Order Order { get; set; }
+        public Tea Tea { get; set; }
+
+    }
+}
